4de625835aa421e142e4a1058109e131e8301862
Huafei Wang <huafeiwang1991@gmail.com>
2016-10-30 23:25:41 -0700

    Initial Commit

---
+++ MapTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MapTest
+//
+//  Created by Huafei Wang on 10/30/16.
+//  Copyright © 2016 Huafei Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapTest
+//
+//  Created by Huafei Wang on 10/30/16.
+//  Copyright © 2016 Huafei Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
