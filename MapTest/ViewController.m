b60eff48199e67e49fd5dc3a514c50d9444f411d
Huafei Wang <huafeiwang1991@gmail.com>
2016-11-02 20:25:38 -0700

    local commit

--- MapTest/ViewController.m
+++ MapTest/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,11 +24,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _locationManager = [[CLLocationManager alloc] init];
+    
+    [_locationManager requestWhenInUseAuthorization];
+    
+    self.mapView.delegate = self;
+    
+    self.mapView.showsUserLocation = YES;
+    
+    NSString *base = [[[NSBundle mainBundle] bundleURL] absoluteString];
+    NSString *template = [base stringByAppendingString:@"/US1/{z}/{x}/{y}.png"];
+    
+//    NSString *template = @"https://c.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    
+    MKTileOverlay *overlay = [[MKTileOverlay alloc] initWithURLTemplate:template];
+    overlay.canReplaceMapContent = YES;
+    [self.mapView addOverlay:overlay
+                       level:MKOverlayLevelAboveLabels];
+//    [self.view addSubview:_mapView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView
+           rendererForOverlay:(id<MKOverlay>)overlay {
+    
+    if ([overlay isKindOfClass:[MKTileOverlay class]]) {
+        return [[MKTileOverlayRenderer alloc] initWithTileOverlay:overlay];
+    }
+    return nil;
 }
 
 @end
